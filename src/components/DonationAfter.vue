6be66bac617fc78790591e0f305676b95122ab21
kmgtk <56664447+kmgtk@users.noreply.github.com>
2022-04-24 01:24:51 +0900

    class binding 추가, cartlist component 분리

--- src/components/DonationAfter.vue
+++ src/components/DonationAfter.vue
@@ -2,19 +2,55 @@
   <div class="group_givetop">
     <strong class="tit_givetop"> 응원, 공유하면 기부되는 가장 쉬운 나눔 </strong><!----><!---->
   </div>
+  <div class="group_fundlist">
+    <h4 class="screen_out">모금후기 리스트</h4>
+    <div data-tiara-layer="listing_logic" class="sort_cate">
+<span class="inner_sort_cate">
+<span :class="{box_sorting : true, sort_on : isBoxSortingActive==='모금후기_클릭' }" data-tiara-layer="epilogue" data-tiara-action-name="모금후기_클릭" @click="click_box_sorting('모금후기_클릭')">
+<input type="radio" class="inp_sort" name="sort0">
+<label class="lab_sort" for="sort0">모금후기</label>
+</span>
+<span :class="{box_sorting : true, sort_on : isBoxSortingActive==='사업중' }" @click="click_box_sorting('사업중')" >
+<input type="radio" class="inp_sort" name="sort1">
+<label class="lab_sort" for="sort1">사업중</label>
+</span>
+  <!---->
+</span>
+    </div>
+
+    <DonationListCard></DonationListCard>
+
+    <!---->
+    <!---->
+    <div class="list_more">
+      <!---->
+    </div>
+    <!---->
+  </div>
+
 </template>
 
 <script>
+
+import DonationListCard from "@/components/DonationListCard";
     export default {
         name: "DonationAfter",
+
+      components:{
+        DonationListCard,
+      },
+
         data() {
             return {
-
+              isBoxSortingActive : '모금후기_클릭',
 
             };
         },
 
         methods :{
+          click_box_sorting(box_sorting_name){
+            this.isBoxSortingActive = box_sorting_name;
+          }
 
 
         },
--- src/components/DonationIng.vue
+++ src/components/DonationIng.vue
@@ -4,7 +4,7 @@
         <div class="inner_catelist">
             <ul class="list_cate">
                 <li v-bind:class="{ on: isActive==='전체'}">
-                    <a data-tiara-layer="all" data-tiara-action-name="토픽_전체_클릭" class="link_cate"  @click="aa('전체')">
+                    <a data-tiara-layer="all" data-tiara-action-name="토픽_전체_클릭" class="link_cate"  @click="clickTopic('전체')">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" aria-hidden="true">
                             <image x="0" y="0" width="100%" height="100%" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="//t1.kakaocdn.net/together_image/ico_all.png">
                             </image>
@@ -13,7 +13,7 @@
                     </a>
                 </li>
                 <li v-bind:class="{ on: isActive==='어린이'}">
-                    <a class="link_cate" data-tiara-layer="children" data-tiara-action-name="토픽_children_클릭"  @click="aa('어린이')">
+                    <a class="link_cate" data-tiara-layer="children" data-tiara-action-name="토픽_children_클릭"  @click="clickTopic('어린이')">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" aria-hidden="true">
                             <image x="0" y="0" width="100%" height="100%" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="https://t1.kakaocdn.net/together_image/ico_kidz.png">
                             </image>
@@ -22,21 +22,21 @@
                     </a>
                 </li>
                 <li v-bind:class="{ on: isActive==='여성'}">
-                    <a class="link_cate" data-tiara-layer="woman" data-tiara-action-name="토픽_woman_클릭" @click="aa('여성')">
+                    <a class="link_cate" data-tiara-layer="woman" data-tiara-action-name="토픽_woman_클릭" @click="clickTopic('여성')">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" aria-hidden="true">
                             <image x="0" y="0" width="100%" height="100%" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="https://t1.kakaocdn.net/together_image/ico_woman.png">
                             </image>
                         </svg>
-                        <span  class="txt_cate">여성</span>
+                        <span  class="txt_cate" @click="clickTopic('여성')">여성</span>
                     </a>
                 </li>
                 <li v-bind:class="{ on: isActive==='어르신'}">
-                    <a class="link_cate" data-tiara-layer="silver" data-tiara-action-name="토픽_silver_클릭" @click="aa('어르신')">
+                    <a class="link_cate" data-tiara-layer="silver" data-tiara-action-name="토픽_silver_클릭" >
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" aria-hidden="true">
                             <image x="0" y="0" width="100%" height="100%" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="https://t1.kakaocdn.net/together_image/ico_old.png">
                             </image>
                         </svg>
-                        <span  class="txt_cate">어르신</span>
+                        <span  class="txt_cate" @click="clickTopic('어르신')">어르신</span>
                     </a>
                 </li>
             </ul>
@@ -57,17 +57,17 @@
       모금중 리스트</h4>
     <div data-tiara-layer="listing_logic" class="sort_cate">
 <span class="inner_sort_cate">
-<span class="box_sorting sort_on" data-tiara-layer="recommend" data-tiara-action-name="추천순_클릭">
+<span :class="{box_sorting : true, sort_on : isBoxSortingActive==='추천순_클릭' }" data-tiara-layer="recommend" data-tiara-action-name="추천순_클릭" @click="click_box_sorting('추천순_클릭')">
 <input type="radio" class="inp_sort" name="sort0">
 <label class="lab_sort" for="sort0">
 추천순</label>
 </span>
-<span class="box_sorting" data-tiara-layer="recent" data-tiara-action-name="최신순_클릭">
+<span :class="{box_sorting : true, sort_on : isBoxSortingActive==='최신순_클릭' }" data-tiara-layer="recent" data-tiara-action-name="최신순_클릭" @click="click_box_sorting('최신순_클릭')">
 <input type="radio" class="inp_sort" name="sort1">
 <label class="lab_sort" for="sort1">
 최신순</label>
 </span>
-<span class="box_sorting" data-tiara-layer="complete" data-tiara-action-name="종료임박순_클릭">
+<span :class="{box_sorting : true, sort_on : isBoxSortingActive==='종료임박순_클릭' }" data-tiara-layer="complete" data-tiara-action-name="종료임박순_클릭" @click="click_box_sorting('종료임박순_클릭')">
 <input type="radio" class="inp_sort" name="sort2">
 <label class="lab_sort" for="sort2">
 종료임박순</label>
@@ -75,102 +75,27 @@
   <!---->
 </span>
     </div>
-    <!---->
-    <ul class="list_fund fund_raising">
-      <!---->
-      <li class="listcard" v-for="(cardData, index) in cardDatas" :key=index>
-        <intersection-observer>
-          <fundraising-card>
-            <a data-tiara-layer="p_card" data-tiara-action-name="프로젝트모금함_카드_클릭" class="link_pack" :href="'/fundraisings/'+cardData.postid">
-<span class="box_thumb">
-<span kagetype="c203" class="img_thumb" style="background-image: url(&quot;https://mud-kage.kakaocdn.net/dn/bn3wWP/btrw63EPYSj/S3jhP3AQbsV3Ib0qOyrvr0/c203.jpg&quot;);">
-</span>
-</span>
-              <span class="box_together">
-<span class="bundle_tit">
-<strong class="tit_together ellipsis_type1">
-<span class="tag_bundle"></span>
- {{ cardData.title }} </strong>
-<span class="">
-<span class="screen_out">제안자</span>
-{{ cardData.proponent }}</span>
-</span>
-<span class="wrap_state">
-<span class="state_bar">
-<span class="state_gage state_ing" :style="{width : cardData.achievement+'%'}"></span>
-</span>
-<span class="screen_out">달성률</span>
-<span class="txt_per">{{cardData.achievement}}%</span>
-</span>
-
-<span class="price_goal">{{cardData.price}} </span>
-</span>
-            </a>
-
-          </fundraising-card>
-        </intersection-observer>
-      </li>
-    </ul>
+    <DonationListCard></DonationListCard>
   </div>
 
 
 
 </template>
 
+
 <script>
+
+import DonationListCard from "@/components/DonationListCard";
+
     export default {
         name: "DonationIng",
+      components:{
+        DonationListCard,
+      },
         data() {
             return {
-              cardDatas : [
-                {
-                  title: '가',
-                  proponent: '가가',
-                  achievement: 10,
-                  price: 10000,
-                  postid:1
-                },
-                {
-                  title: '나',
-                  proponent: '나나',
-                  achievement: 20,
-                  price: 20000,
-                  postid:2
-                },
-                {
-                  title: '다',
-                  proponent: '다다',
-                  achievement: 30,
-                  price: 30000,
-                  postid:3
-                },
-                {
-                  title: '라',
-                  proponent: '라라',
-                  achievement: 40,
-                  price: 40000,
-                  postid:4
-                },
-                {
-                  title: '마',
-                  proponent: '마마',
-                  achievement: 50,
-                  price: 50000
-                },
-                {
-                  title: '마',
-                  proponent: '마마',
-                  achievement: 50,
-                  price: 50000
-                },
-                {
-                  title: '마',
-                  proponent: '마마',
-                  achievement: 50,
-                  price: 50000
-                },
-              ],
               isActive : '전체',
+              isBoxSortingActive : '추천순_클릭',
             };
         },
 
@@ -183,10 +108,13 @@
       },
 
         methods :{
-          aa(topic){
+          clickTopic(topic){
             this.isActive = topic;
             console.log(topic);
           },
+          click_box_sorting(box_sorting_name){
+            this.isBoxSortingActive = box_sorting_name;
+          }
 
         },
     };
